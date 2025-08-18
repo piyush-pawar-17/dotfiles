2a861e3b40e28196114e4c817d3d9e380dc1fbe1
Piyush Pawar <piyushpawar25@gmail.com>
2025-08-18 16:15:24 +0000

    chore: update aliases and shortcuts for GlazeWM

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,6 @@
 alias v="nvim"
 alias bat=batcat
 alias lg=lazygit
-alias t=tmux
-alias install="sudo apt-get install"
 
 # Listing files
 alias ls="eza -F --color=always --group-directories-first"
@@ -21,7 +19,7 @@ function take {
 
 # Git
 alias gbdf="git branch | fzf -m | xargs git branch -D"
-alias gcof="git branch | fzf | xargs git checkout"
+alias gco="git branch | fzf | xargs git checkout"
 alias glc='git pull origin $(git branch --show-current)'
 alias glo="git log --oneline --pretty=format:'%C(bold yellow)%h%C(reset)%C(reset)%C(auto)%d%C(reset) - %C(white)%s%C(reset) %C(dim white)- %an (%ar)'"
 alias gsp='git reflog | grep ".*checkout: moving from.*to.*$(git branch --show-current)" | tail -n1 | sed "s/.*checkout: moving from \(.*\) to.*/\1/"'
